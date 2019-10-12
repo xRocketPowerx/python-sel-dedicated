50d8e361980b075a538e9c6ee2a2a839eb342d3a
Максим Спиридонов <xmax.sox@gmail.com>
2019-10-12 14:55:56 +0300

    add empty test

---
+++ tests/first_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import sel_dedicated
+
+class TestName(unittest.TestCase):
+    """Name of unitTest"""
+
+
+    def testName(self):
+        """Test Name"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
